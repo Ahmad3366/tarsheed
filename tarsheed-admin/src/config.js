2d78210bd2f0f2757b890b9da3869f4ca90a9440
Ahmed <ahmaddx900@gmail.com>
2025-07-11 14:59:12 +0200

    Update API_URL to use production endpoint

--- tarsheed-admin/src/config.js
+++ tarsheed-admin/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000';
-// export const API_URL = "https://tarsheed-5nms.onrender.com";
+// export const API_URL = 'http://localhost:3000';
+export const API_URL = "https://tarsheed-5nms.onrender.com";
