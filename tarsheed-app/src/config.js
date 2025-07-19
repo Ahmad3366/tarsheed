5b14aab4bf753164b9ae4517c8cef9b1e181438c
Ahmed-Elmahe <officialalmahui@gmail.com>
2025-07-20 01:45:58 +0300

    finshing adding all my feature

--- tarsheed-app/src/config.js
+++ tarsheed-app/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api/reports';
+// export const API_URL = 'http://localhost:8000/api/reports';
 // export const API_URL = "https://tarsheed-5nms.onrender.com/api/reports";
